40d6f71dc1a868c263f2b0cb0e475979fa459f30
Juan G. Llanos <juanguillanos@gmail.com>
2017-06-15 12:59:02 -0400

    moved utility functions into module

---
+++ llanos/KL_divergence/utils.js
@@ -0,0 +1,50 @@
+function linspace(start, stop, numPoints){
+    var delta = (stop - start)/(numPoints-1)
+    var samples = []
+    for (var i = 0; i < numPoints; i++){
+      samples.push(start + i*delta); 
+    }
+    return samples;
+  }
+
+  function gaussianPDF(x, mean, std) {
+    var gaussianConstant = 1 / Math.sqrt(2 * Math.PI);
+    if (Array.isArray(x)){
+      var numPoints = x.length;
+      var pdf = [];
+      for (var i = 0; i < numPoints; i++){
+        var exponent = (x[i] - mean) / std;
+        pdf.push(gaussianConstant * Math.exp(-.5 * exponent * exponent) / std); 
+      }
+      return pdf;
+    } else {
+      var exponent = (x - mean) / std;
+      return gaussianConstant * Math.exp(-.5 * exponent * exponent) / std;
+    }
+  }
+
+  function klDiv(p, q) {
+    if (p.length != q.length) {
+      return 0;
+    }
+    var numPoints = p.length;
+    var kl = 0;
+    for (var i = 0; i < numPoints; i++) {
+      kl = kl + p[i] * Math.log(p[i]/q[i])
+    }
+    return kl;
+  }
+  
+  function sumArrays(a, b) {
+    
+    if (a.length != b.length) {
+      return 0;
+    }
+
+    var numPoints = a.length;
+    var sum = [];
+    for (var i = 0; i < numPoints; i++) {
+      sum.push(a[i] + b[i]);
+    }
+    return sum;
+  }
